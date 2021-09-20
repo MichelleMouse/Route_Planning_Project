65f7f776e2edf2e13f19089b7422a624f24d276f
Michelle Husbands Correa <mrhc93@gmail.com>
2021-09-20 15:27:56 +1000

    Update variables.zsh

--- variables.zsh
+++ variables.zsh
@@ -1,3 +1,3 @@
 #Project flags
-export LDFLAGS="-L/Users/YOUR_USER/GitHub/Route_Planning_Project/thirdparty/clang/lib -Wl,-rpath,/Users/YOUR_USER/GitHub/Route_Planning_Project/thirdparty/clang/lib -L/opt/X11/lib -L/opt/local/lib -L/usr/local/lib"
-export CXXFLAGS="-nostdinc++ -isystem/Users/YOUR_USER/GitHub/Route_Planning_Project/thirdparty/clang/include/c++/v1 -isystem/opt/X11/include -isystem/opt/local/include -isystem/usr/local/include"
+export LDFLAGS="-L/Users/{$USER}/GitHub/Route_Planning_Project/thirdparty/clang/lib -Wl,-rpath,/Users/{$USER}/GitHub/Route_Planning_Project/thirdparty/clang/lib -L/opt/X11/lib -L/opt/local/lib -L/usr/local/lib"
+export CXXFLAGS="-nostdinc++ -isystem/Users/{$USER}/GitHub/Route_Planning_Project/thirdparty/clang/include/c++/v1 -isystem/opt/X11/include -isystem/opt/local/include -isystem/usr/local/include"
